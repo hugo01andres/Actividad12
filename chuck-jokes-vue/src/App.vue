2f411e4a8d9959d0af46686bfd2d6eb004d75b5f
Hugo Martinez <72633652+hugo01andres@users.noreply.github.com>
2024-04-21 04:29:18 +0000

    Creo la actividad 12

---
+++ chuck-jokes-vue/src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="app">
+    <h1>Chuck Norris Jokes</h1>
+    <ul>
+      <li v-for="(joke, index) in chuckJokes" :key="index">{{ joke.value }}</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      chuckJokes: [
+        { value: "Chuck Norris can skydive into outer space." },
+        { value: "The chief export of Chuck Norris is pain." },
+        { value: "Chuck Norris doesn't read books. He stares them down until he gets the information he wants." },
+        { value: "Time waits for no man. Unless that man is Chuck Norris." },
+        { value: "If you spell Chuck Norris in Scrabble, you win. Forever." },
+      ]
+    };
+  }
+};
+</script>
+
+<style>
+/* Agrega estilos según tu preferencia */
+</style>
+clear
